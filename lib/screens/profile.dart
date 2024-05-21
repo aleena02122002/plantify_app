1ee3057f247c9ba8d8b85bc2f50d1aaf6758caec
aleena02122002 <khananna919@gmail.com>
2024-05-21 23:27:34 +0500

    profile page addition, icons addition

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:plants/screens/order.dart';
+import 'package:plants/screens/setting.dart';
+import 'package:plants/widgets/drawer.dart';
+import 'package:plants/widgets/popupMenu.dart';
+import 'package:plants/widgets/profilePageListTile.dart';
 
 class MyProfile extends StatelessWidget {
   const MyProfile({super.key});
@@ -7,49 +12,107 @@ class MyProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: CustomDrawer(),
+      appBar: AppBar(
+        title: Container(
+            height: 100,
+            width: 100,
+            child: Image.asset('images/Logo.png', fit: BoxFit.contain)),
+        actions: <Widget>[
+          IconButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => OrderRecieved()));
+              },
+              icon: Icon(Icons.notification_add)),
+          Popup(),
+          // IconButton(onPressed: ()=> _key.currentState!.openDrawer(), icon: Icon(Icons.drag_handle_outlined)),
+        ],
+      ),
       body: SingleChildScrollView(
         child: Container(
-          padding: EdgeInsets.symmetric(vertical: 100.0, horizontal: 120.0),
+
           child: Column(
             children: [
-              SizedBox(
-                width: 120,
-                height: 120,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(100),
-                  child: Image(
-                    image: AssetImage('images/profile2.jpg'),
-                    width: 400,
-                  ),
+              SizedBox(height: 10,),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 120.0,),
+                child: Stack(
+                  children: [
+                    SizedBox(
+                      width: 120,
+                      height: 120,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: const Image(
+                          image: AssetImage('images/profile2.jpg'),
+                          width: 400,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        width: 35,
+                        height: 35,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: Color(0xFF0D986A).withOpacity(0.4),
+                        ),
+                        child: Icon(Icons.edit, color: Color(0xFF0D986A)),
+                      ),
+                    ),
+                  ],
                 ),
               ),
               const SizedBox(
                 height: 10,
               ),
-              Text(
+              const Text(
                 'Name',
                 style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
               ),
-              FittedBox(
+              const FittedBox(
                 child: Text(
                   'addressID@gmail.com',
-                  style: TextStyle(fontSize: 20),
+                  style: TextStyle(fontSize: 17),
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 15,
               ),
               SizedBox(
                 width: 200,
                 child: ElevatedButton(
                   onPressed: () {},
-                  child: Text('Edit Profile'),
+                  child: const Text('Edit Profile'),
                   style: ElevatedButton.styleFrom(backgroundColor:const Color(0xFF0D986A), foregroundColor: Colors.white,  shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10.0),
                   ),
                 ),
                 )
-              )
+              ),
+              const SizedBox(height: 30,),
+              // Divider(
+              //   color: Color(0xFF0D986A),
+              // ),
+              const SizedBox(height: 10,),
+              ProfileMenu(title: 'Setting', iconData: Icons.settings, voidCallback: (){
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => Setting()));
+              }),
+              ProfileMenu(title: 'Billing Details', iconData: Icons.wallet, voidCallback:(){}),
+              ProfileMenu(title: 'Promotion', iconData: Icons.sell_sharp, voidCallback: (){}),
+              Divider(
+                indent: 50,
+                endIndent: 50,
+              ),
+              SizedBox(),
+              ProfileMenu(title: 'About', iconData: Icons.info, voidCallback: (){}),
+              ProfileMenu(title: 'LogOut', iconData: Icons.logout, voidCallback: (){},textColor: Colors.red,endIcon: false,)
             ],
           ),
         ),
